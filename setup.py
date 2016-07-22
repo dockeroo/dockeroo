de98a7c826fce907ab4ee4baf948520c6f734a52
Giacomo Cariello <g.cariello@ieee.org>
2016-07-22 18:13:20 +0200

    Bumped version to 0.6.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
-VERSION = '0.5'
+VERSION = '0.6'
 
 requires = [
     'setuptools',
